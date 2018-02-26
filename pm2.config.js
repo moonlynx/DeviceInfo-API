ab694f183c058cbbb48052040241437c6eeb8302
moonlynx <moonlynx@yandex.ru>
2018-02-26 13:28:47 +0300

    add cluster mode in pm2.config file

--- pm2.config.js
+++ pm2.config.js
@@ -2,9 +2,13 @@ module.exports = {
   apps : [{
     name   : "DeviceInfo-API",
     script : "./app/server.js",
+        
+    exec_mode : "cluster",
+    instances : "2",
+
     log_date_format: "DD-MM-YYYY HH:mm:ss",
     error_file: "./logs/server_error.log",
     out_file: "./logs/server.log",
-    merge_logs: true   
+    merge_logs: true
   }]
 }
